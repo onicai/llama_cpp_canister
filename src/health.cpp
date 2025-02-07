f45d53ac1518df92a111d3d237cc79684f01b7d6
icpp <icpp@icpp.world>
2025-02-07 14:38:36 -0500

    Do not print anything in the health() endpoint

--- src/health.cpp
+++ src/health.cpp
@@ -9,8 +9,6 @@
 void health() {
   IC_API ic_api(CanisterQuery{std::string(__func__)}, false);
 
-  std::cout << "Canister is healthy!" << std::endl;
-
   CandidTypeRecord status_code_record;
   status_code_record.append("status_code", CandidTypeNat16{200});
   ic_api.to_wire(CandidTypeVariant{"Ok", status_code_record});
